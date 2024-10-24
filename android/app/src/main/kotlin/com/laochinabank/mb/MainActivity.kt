fb0bb02a48c336645d4470c35f9a87c664a03901
persilee <i@lishaoy.net>
2024-10-25 00:59:26 +0800

    🍉 add location

--- android/app/src/main/kotlin/com/laochinabank/mb/MainActivity.kt
+++ android/app/src/main/kotlin/com/laochinabank/mb/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.laochinabank.mb
 
 
+import android.Manifest
 import android.content.ContentValues.TAG
+import android.content.pm.PackageManager
 import android.os.Looper
 import android.util.Log
 import com.huawei.hmf.tasks.OnFailureListener
@@ -16,7 +18,12 @@ import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import android.location.Location
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import com.google.gson.Gson
+import com.huawei.hms.location.HWLocation
 import com.huawei.hms.location.LocationAvailability
+
 
 
 private const val HMS_LOCATION = "com.laochinabank.mb/hms_location"
@@ -33,6 +40,44 @@ class MainActivity : FlutterFragmentActivity() {
         channel?.setMethodCallHandler { call, result ->
             when (call.method) {
                 "getLocation" -> {
+                    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                        Log.i(TAG, "android sdk <= 28 Q")
+                        if (ActivityCompat.checkSelfPermission(
+                                this,
+                                Manifest.permission.ACCESS_FINE_LOCATION
+                            ) != PackageManager.PERMISSION_GRANTED
+                            && ActivityCompat.checkSelfPermission(
+                                this,
+                                Manifest.permission.ACCESS_COARSE_LOCATION
+                            ) != PackageManager.PERMISSION_GRANTED
+                        ) {
+                            val strings = arrayOf(
+                                Manifest.permission.ACCESS_FINE_LOCATION,
+                                Manifest.permission.ACCESS_COARSE_LOCATION
+                            )
+                            ActivityCompat.requestPermissions(this, strings, 1)
+                        }
+                    } else {
+                        // Android SDK > 28 所需权限动态申请，需添加“android.permission.ACCESS_BACKGROUND_LOCATION”权限。
+                        if (ActivityCompat.checkSelfPermission(
+                                this,
+                                Manifest.permission.ACCESS_FINE_LOCATION
+                            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                                this,
+                                Manifest.permission.ACCESS_COARSE_LOCATION
+                            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                                this,
+                                "android.permission.ACCESS_BACKGROUND_LOCATION"
+                            ) != PackageManager.PERMISSION_GRANTED
+                        ) {
+                            val strings = arrayOf(
+                                Manifest.permission.ACCESS_FINE_LOCATION,
+                                Manifest.permission.ACCESS_COARSE_LOCATION,
+                                "android.permission.ACCESS_BACKGROUND_LOCATION"
+                            )
+                            ActivityCompat.requestPermissions(this, strings, 2)
+                        }
+                    }
                     val settingsClient = LocationServices.getSettingsClient(this)
                     val builder = LocationSettingsRequest.Builder()
                     val mLocationRequest = LocationRequest()
@@ -46,65 +91,57 @@ class MainActivity : FlutterFragmentActivity() {
                         .addOnSuccessListener(OnSuccessListener { locationSettingsResponse ->
                             val locationSettingsStates =
                                 locationSettingsResponse.locationSettingsStates
-                            val stringBuilder = StringBuilder()
-                            // 定位开关是否打开
-                            stringBuilder.append("isLocationUsable=")
-                                .append(locationSettingsStates.isLocationUsable)
-                            // HMS Core是否可用
-                            stringBuilder.append(",\nisHMSLocationUsable=")
-                                .append(locationSettingsStates.isHMSLocationUsable)
-                            Log.i("TAG", "checkLocationSetting onComplete:$stringBuilder")
+                            // HMS Core是否可用 」 定位开关是否打开
+                            if (locationSettingsStates.isLocationUsable && locationSettingsStates.isHMSLocationUsable) {
+                                // 获取FusedLocationProviderClient实例
+                                mFusedLocationProviderClient =
+                                    LocationServices.getFusedLocationProviderClient(this)
 
+                                // 设置位置回调的时间间隔为6000ms，默认是5000ms。
+                                mLocationRequest.setInterval(6000)
 
-                            // 获取FusedLocationProviderClient实例
-                            mFusedLocationProviderClient =
-                                LocationServices.getFusedLocationProviderClient(this)
+                                // 设置定位类型，PRIORITY_HIGH_ACCURACY为融合定位模式。
+                                mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
 
-                            // 设置位置回调的时间间隔为6000ms，默认是5000ms。
-                            mLocationRequest.setInterval(6000)
+                                // 设置回调次数为100，默认值为Integer.MAX_VALUE；。
+                                // 特殊情况：单次定位场景，只需设置回调次数为1，仅会发起一次定位，定位结束之后，也不需要移除定位请求。
+                                mLocationRequest.setNumUpdates(1)
+                                if (null == mLocationCallback) {
+                                    mLocationCallback = object : LocationCallback() {
+                                        override fun onLocationResult(locationResult: LocationResult?) {
+                                            if (locationResult != null) {
 
-                            // 设置定位类型，PRIORITY_HIGH_ACCURACY为融合定位模式。
-                            mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                                                val location:HWLocation = locationResult.lastHWLocation
+                                                result.success(Gson().toJson(location))
+                                            }
+                                        }
 
-                            // 设置回调次数为100，默认值为Integer.MAX_VALUE；。
-                            // 特殊情况：单次定位场景，只需设置回调次数为1，仅会发起一次定位，定位结束之后，也不需要移除定位请求。
-                            mLocationRequest.setNumUpdates(10)
-                            if (null == mLocationCallback) {
-                                mLocationCallback = object : LocationCallback() {
-                                    override fun onLocationResult(locationResult: LocationResult?) {
-                                        if (locationResult != null) {
-                                            val locations: List<Location> =
-                                                locationResult.getLocations()
-                                            if (locations.isNotEmpty()) {
-                                                for (location in locations) {
-                                                    Log.i(
-                                                        TAG,
-                                                        "onLocationResult location[Longitude,Latitude,Accuracy]:${location.longitude} , ${location.latitude} , ${location.accuracy}"
-                                                    )
-                                                }
+                                        override fun onLocationAvailability(locationAvailability: LocationAvailability?) {
+                                            locationAvailability?.let {
+                                                val flag: Boolean =
+                                                    locationAvailability.isLocationAvailable
+                                                Log.i(
+                                                    TAG,
+                                                    "onLocationAvailability isLocationAvailable:$flag"
+                                                )
                                             }
                                         }
                                     }
-
-                                    override fun onLocationAvailability(locationAvailability: LocationAvailability?) {
-                                        locationAvailability?.let {
-                                            val flag: Boolean = locationAvailability.isLocationAvailable
-                                            Log.i(TAG, "onLocationAvailability isLocationAvailable:$flag")
-                                        }
-                                    }
                                 }
+                                // 发起定位
+                                mFusedLocationProviderClient
+                                    .requestLocationUpdates(
+                                        mLocationRequest,
+                                        mLocationCallback,
+                                        Looper.getMainLooper()
+                                    );
                             }
-                            // 发起定位
-                            mFusedLocationProviderClient
-                                .requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.getMainLooper());
+
                         })
                         // 检查设备定位设置接口失败监听回调
                         .addOnFailureListener(OnFailureListener { e ->
                             Log.i("TAG", "checkLocationSetting onFailure:" + e.message)
                         })
-
-
-                    result.success(true)
                 }
             }
         }
--- lib/native_service.dart
+++ lib/native_service.dart
@@ -1,8 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/services.dart';
 
 const nativeChannel = MethodChannel('com.laochinabank.mb/hms_location');
 
 Future<String> getLocation() async {
   final result = await nativeChannel.invokeMethod("getLocation");
+  print('11111111111: $result');
+  Map data = jsonDecode(result);
+  print('aaaaaaaaaaaa: $data');
   return result;
 }
